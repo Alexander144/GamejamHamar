cd5d0e51ec143e7e6d510d048406b03c6f884aa0
Kevin Aslan <kevin_aslan@hotmail.com>
2016-01-30 01:32:34 +0100

    Fix - bug

--- BestCardGameEver/Hamar Game Jam 2016/Assets/Scripts/GM.cs
+++ BestCardGameEver/Hamar Game Jam 2016/Assets/Scripts/GM.cs
@@ -3,17 +3,12 @@ using System.Collections;
 
 public class GM : MonoBehaviour {
 
-    public Contestant _hero;
-    public Contestant _villain;
-    public Board _board;
-    public Input _input;
+    private int _playerHP;
+    private int _enemyHP;
 
 	// Use this for initialization
 	void Start () {
-        _board = new Board();
-        _hero = new C_Hero();
-        _villain = new C_Villain();
-        _input = new Input();
+	
 	}
 	
 	// Update is called once per frame
